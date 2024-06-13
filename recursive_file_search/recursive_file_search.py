2c7174e98089ce80d59e7be7b4ed34a75f243d28
Hullryans11 <hullryans11@gmail.com>
2024-06-13 14:14:12 +0200

    Add files via upload

---
+++ recursive_file_search/recursive_file_search.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+class dir_node:
+#Class for dir objects, should have a directory location,
+#array of child nodes and array of child files
+
+    str dir_path
+    dir_children[]
+    file_children[]
+    
+    
+    def search
+    
