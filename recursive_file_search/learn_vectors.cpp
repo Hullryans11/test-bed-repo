ad664714ea6d260a73c09268d342f58ab373d317
Hullryans11 <hullryans11@gmail.com>
2024-06-18 11:36:43 +0200

    Update learn_vectors.cpp

--- recursive_file_search/learn_vectors.cpp
+++ recursive_file_search/learn_vectors.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 int main(){
-  
+  int a = 10
   //init requires data type in <> name, size not required because it dyanmic
   std::vector<int> vecT; // or you can = 0,1,2,3};
   vecT = { 0, 1, 2, 3};
@@ -10,6 +10,17 @@ int main(){
   std::cout << vecT.at(3);
   vecT.at(3) = 2;
   std::cout << vecT.at(3);
+
+  //show size
+  std::cout << vecT.size();
+
+  // add an elemnt to the end
+  vecT.push_back(a);
+  //insert an element insert()
+  //remove last element = .pop_back()
+  //remove a specified element or range based on index .erase(va or .erase(index, index+4)
+  
+  
   
 
   return 0;
