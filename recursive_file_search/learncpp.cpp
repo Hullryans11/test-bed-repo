aa407215b6d260b6497c73f65c4dde92abb7edde
Hullryans11 <hullryans11@gmail.com>
2024-06-18 11:08:56 +0200

    Update learncpp.cpp

--- recursive_file_search/learncpp.cpp
+++ recursive_file_search/learncpp.cpp
@@ -1,11 +1,16 @@
 //libs...
-#includes <iostream>
+#include <iostream>
+
+
+//constants, cant be modified in the future... should be something that sho9uld never be changed, like minutes in an hour or somthing
+//Usually declared before any functions in classes and main
+const int min_per_hour = 60;
+
 
 int main()
 {
 	//int examples rounded positive and neg numbers
 	int a = 2;
-  a = -3;
 
 	//doubles exapmples
 	double b;
@@ -16,11 +21,13 @@ int main()
 	y = false;
 
 	//string type C++ only, must use char* array in c
-	std:string s;
+	std::string s;
 	s = "string content";
+
+	
 	
 	
   
-  std::cout << "Text";
+  std::cout << "Text" << b << "\n" << y << "\n" << s;
 return 0;
 }
