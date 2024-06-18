5d434d75bfc5f514685c40f64dc9fcfa2aa70f60
Hullryans11 <hullryans11@gmail.com>
2024-06-18 13:53:40 +0200

    Update functions.cpp

--- recursive_file_search/functions.cpp
+++ recursive_file_search/functions.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 
+
+//Function baiscs
+//Void id the return type of the function
+void print_hello(){
+  std::cout <<"Hello";
+}
+
+
+
+
 int main(){
   
-
+  print_hello();
+  
 
 
 
   return 0;
 }
+
