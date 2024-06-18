02f04246bde1743c407922d5866a7cbaf279b0fe
Hullryans11 <hullryans11@gmail.com>
2024-06-18 11:15:30 +0200

    Update learncpp.cpp

--- recursive_file_search/learncpp.cpp
+++ recursive_file_search/learncpp.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 
+
 //constants, cant be modified in the future... should be something that sho9uld never be changed, like minutes in an hour or somthing
 //Usually declared before any functions in classes and main
 const int min_per_hour = 60;
@@ -23,6 +24,19 @@ int main()
 	//string type C++ only, must use char* array in c
 	std::string s;
 	s = "string content";
+
+	//Arrays must be init with a fioxed size or with the compnents
+
+	double temp[4];
+	//or
+	int temp2[4] = { 0, 1, 2, 3};
+	//modifiy array like normal by index
+	temp2[3] = 3;
+	
+	//vectors!!! in stad C== library bsically a dynamic array implimentation in the STD library
+	//# include <vector>
+	
+	
 
 	
 	
