c59dcc11d3e052a50a9f96b558b900796871893a
Hullryans11 <hullryans11@gmail.com>
2024-06-18 12:06:56 +0200

    Update lvl_1_exc.cpp

--- recursive_file_search/lvl_1_exc.cpp
+++ recursive_file_search/lvl_1_exc.cpp
@@ -3,16 +3,26 @@
 
 int main(){
 
-  // 1.1 Ask for name and ange then output
+// 1.1 Ask for name and ange then output
 
+  //init used vars
   std::string name;
   int age;
 
+  //ask name and recieve input
   std::cout << "\nPlease enter your name: ";
   std::cin >> name;
-  //1.2 Ask for two integers, add then print the reult
 
-  // recive 4 numbers from user, input into a vector, comput the average and print it (while loop or for loop?)
+  //ask age and recive input
+  std::cout << "\nPlease enter your age: ";
+  std::cin >> age;
+
+  //Print info
+  std::cout<< "Your name is " << name << " and your age is " << age << std::endl;
+  
+//1.2 Ask for two integers, add then print the reult
+
+// recive 4 numbers from user, input into a vector, comput the average and print it (while loop or for loop?)
 
   
 
