77b985d1070e234945f4d39a8cf683d9dbfa3977
Hullryans11 <hullryans11@gmail.com>
2024-06-18 11:01:07 +0200

    Update learncpp.cpp

--- recursive_file_search/learncpp.cpp
+++ recursive_file_search/learncpp.cpp
@@ -3,8 +3,23 @@
 
 int main()
 {
-  int a = 2;
-  a = 3;
+	//int examples rounded positive and neg numbers
+	int a = 2;
+  a = -3;
+
+	//doubles exapmples
+	double b;
+	b = -35.4;
+
+	//boolean type true ot false, no cap
+	bool y;
+	y = false;
+
+	//string type C++ only, must use char* array in c
+	std:string s;
+	s = "string content";
+	
+	
   
   std::cout << "Text";
 return 0;
