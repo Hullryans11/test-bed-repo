c75b8cfdb21d0b049fe74f23bf61a649049d838f
Hullryans11 <hullryans11@gmail.com>
2024-06-18 12:03:50 +0200

    Update lvl_1_exc.cpp

--- recursive_file_search/lvl_1_exc.cpp
+++ recursive_file_search/lvl_1_exc.cpp
@@ -8,6 +8,8 @@ int main(){
   std::string name;
   int age;
 
+  std::cout << "\nPlease enter your name: ";
+  std::cin >> name;
   //1.2 Ask for two integers, add then print the reult
 
   // recive 4 numbers from user, input into a vector, comput the average and print it (while loop or for loop?)
