257c1d71ebe4dbb26245ed231840476b9ca6a469
Hullryans11 <hullryans11@gmail.com>
2024-06-18 11:27:57 +0200

    Update learn_vectors.cpp

--- recursive_file_search/learn_vectors.cpp
+++ recursive_file_search/learn_vectors.cpp
@@ -4,10 +4,13 @@
 int main(){
   
   //init requires data type in <> name, size not required because it dyanmic
-  std::vector<int> vecT;
+  std::vector<int> vecT; // or you can = 0,1,2,3};
   vecT = { 0, 1, 2, 3};
+  //access an element of a vector using the .at() func with the index
+  std::cout << vecT.at(3);
+  vecT.at(3) = 2;
+  std::cout << vecT.at(3);
   
-
 
   return 0;
 }
