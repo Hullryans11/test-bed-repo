086f8679b1d132fc2f89faf87a13ed459ec201c0
Hullryans11 <hullryans11@gmail.com>
2024-07-03 10:35:51 +0200

    Update db.c

--- learn_c/db.c
+++ learn_c/db.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#define MAX_DATA
+#define MAX_ROWS 100
+
+
+//Defines the components of an address object, will be used a a record in the database
+struct Address{
+int id;
+int set; //
+char name[MAX_DATA];
+char email[];
+};
+
+//Struct that defines the components of a database. capable of holding 100 records of the address object
+struct Database{
+  struct Address rows[MAX_ROWS];
+};
+
+struct
